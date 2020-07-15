9cd6a0131e0ee67c5b7f07407d00cdb19c3c87d5
Minh Duc Le <42980704+DeaLoux@users.noreply.github.com>
2020-07-15 02:31:21 -0500

    database folder

---
+++ src/database/dbInit.js
@@ -0,0 +1,41 @@
+/* This module initialize the database */
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+/* local
+const sequelize = new Sequelize("database", "username", "password", {
+	host: "localhost",
+	dialect: "sqlite",
+	storage: "database.sqlite",
+	logging: false,
+}); */
+
+/* server */
+const sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+	dialect:  "postgres",
+	protocol: "postgres",
+	port:     match[4],
+	host:     match[3],
+	logging:   false
+}); 
+
+const StockMarket = sequelize.import("../database/models/stock_market");
+sequelize.import("../database/models/users");
+sequelize.import("../database/models/guilds");
+sequelize.import("../database/models/user_stocks");
+
+const force = process.argv.includes("--force") || process.argv.includes("-f"); // force sync
+
+sequelize.sync({ force }).then(async () => {
+	const shop = [
+		await StockMarket.upsert({ name: "Casino", cost: 120, market_share: 100 }),
+		await StockMarket.upsert({ name: "Bank", cost: 250, market_share: 100 }),
+		await StockMarket.upsert({ name: "Eastern Law", cost: 400, market_share: 100 }),
+		await StockMarket.upsert({ name: "Western Law", cost: 400, market_share: 100 }),
+		await StockMarket.upsert({ name: "Court", cost: 500, market_share: 100 }),
+		await StockMarket.upsert({ name: "Government", cost: 700, market_share: 100 }),
+	];
+	await Promise.all(shop);
+	console.log("Database synced");
+	sequelize.close();
+}).catch(console.error);
---
+++ src/database/dbObjects.js
@@ -0,0 +1,45 @@
+/* This module creates the associations between the models of the database */
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+/* local
+const sequelize = new Sequelize("database", "username", "password", {
+	host: "localhost",
+	dialect: "sqlite",
+	storage: "database.sqlite",
+	logging: false,
+}); */
+
+/* server */
+const sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+	dialect:  "postgres",
+	protocol: "postgres",
+	port:     match[4],
+	host:     match[3],
+	logging:   false
+}); 
+
+const Users = sequelize.import("../database/models/users");
+const Guilds = sequelize.import("../database/models/guilds");
+const StockMarket = sequelize.import("../database/models/stock_market");
+const UserStocks = sequelize.import("../database/models/user_stocks");
+
+UserStocks.belongsTo(StockMarket, { foreignKey: "stock_id", as: "stock" });
+
+/** Users functions */
+Users.prototype.addStock = async function(stock, amount) {
+	const userStock = await UserStocks.findOne({where: { user_id: this.user_id, stock_id: stock.id },});
+
+	// increase the user_share if the stock is found in the user portfolio, else add the stock to the portfolio
+	if (userStock) { return userStock.increment("user_share", { by: amount }); }
+	await UserStocks.create({ user_id: this.user_id, stock_id: stock.id, user_share: amount });
+};
+
+Users.prototype.getStocks = function() {
+	return UserStocks.findAndCountAll({
+		where: { user_id: this.user_id },
+		include: ["stock"],
+	});
+};
+
+module.exports = { Users, Guilds, StockMarket, UserStocks };
