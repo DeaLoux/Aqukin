7ddb97146b3d183d9a3e45721a197ec56a13bc4f
Minh Duc Le <42980704+DeaLoux@users.noreply.github.com>
2020-07-23 03:07:27 -0500

    server fix

--- src/database/dbInit.js
+++ src/database/dbInit.js
@@ -2,22 +2,22 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-/* local */
+/* local 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
 	host: "localhost",
 	dialect: "postgres",
 	storage: "database.postgres",
 	logging: false,
-}); 
+}); */
 
-/* server 
+/* server */
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect:  "postgres",
 	protocol: "postgres",
 	port:     process.env.PORT,
 	host:     process.env.PROJECT_DOMAIN,
 	logging:   false
-}); */
+}); 
 
 const StockMarket = require("../database/models/stock_market")(sequelize, Sequelize.DataTypes);
 require("../database/models/users")(sequelize, Sequelize.DataTypes);
