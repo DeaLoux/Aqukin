c27f92dc2a86119f5650152a03b23294068d9524
Minh Duc Le <42980704+DeaLoux@users.noreply.github.com>
2020-07-23 19:02:49 -0500

    Live status update

--- src/database/dbInit.js
+++ src/database/dbInit.js
@@ -6,7 +6,6 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
 	host: "localhost",
 	dialect: "postgres",
-	storage: "database.postgres",
 	logging: false,
 }); */
 
